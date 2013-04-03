fd24e90bd5d7de3c80db105c9d2477585ae3bfe3
hagna <natester@gmail.com>
2013-04-03 17:15:17 -0600

    crlf clears the line; everyone will want this feature once they see it

--- main.go
+++ main.go
@@ -16,6 +16,8 @@ func main() {
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
 	termbox.Flush()
 
+	x, y := 0, 10
+	X, _ := termbox.Size()
 loop:
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
@@ -23,6 +25,15 @@ loop:
 			if ev.Key == termbox.KeyCtrlQ {
 				break loop
 			}
+			if x == X {
+				for i:=0; i<X; i++ {
+					termbox.SetCell(i, y, ' ', termbox.ColorWhite, termbox.ColorDefault)
+				}
+				x = 0
+			}
+			termbox.SetCell(x, y, ev.Ch, termbox.ColorWhite, termbox.ColorDefault)
+			termbox.Flush()
+			x += 1
 		}
 
 	}
