f5b2cd931a3314df43141be99e060a5fb1883c05
hagna <nate@securitymetrics.com>
2013-04-04 14:56:09 -0600

    add logging and organize for 3 event loops (wip)

--- main.go
+++ main.go
@@ -2,22 +2,26 @@ package main
 
 import (
 	"github.com/nsf/termbox-go"
+	"time"
+	//"strings"
+	"log"
+	"os"
 )
 
-func main() {
-	err := termbox.Init()
-	if err != nil {
-		panic(err)
+func draw_n(cb []termbox.Cell, x, y, count int, color termbox.Attribute, ch rune) {
+	for i := 0; i < count; i++ {
+		termbox.SetCell(x+i, y, ch, color, termbox.ColorDefault)
 	}
-	defer termbox.Close()
-	
-	termbox.SetInputMode(termbox.InputEsc)
-	
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	termbox.Flush()
+}
 
-	x, y := 0, 10
-	X, _ := termbox.Size()
+type struct Ev {
+	termbox.EventKey
+	ttype string
+}
+
+func keyb() chan string {
+	res := make(chan string)
+	go func() {
 loop:
 	for {
 		switch ev := termbox.PollEvent(); ev.Type {
@@ -25,16 +29,67 @@ loop:
 			if ev.Key == termbox.KeyCtrlQ {
 				break loop
 			}
-			if x == X {
-				for i:=0; i<X; i++ {
-					termbox.SetCell(i, y, ' ', termbox.ColorWhite, termbox.ColorDefault)
-				}
-				x = 0
-			}
-			termbox.SetCell(x, y, ev.Ch, termbox.ColorWhite, termbox.ColorDefault)
-			termbox.Flush()
-			x += 1
+			res <- ev
+		}
+		log.Println("done with switch")
+		select {
+		case <-c:
+			log.Println("booom")
+/*			n := len(strings.Fields(string(buf)))
+			color := termbox.ColorWhite + termbox.Attribute(n/X)
+			c := n / 10
+			draw_n(cb, 0, Y-1, c, color, '#')
+			termbox.Flush()*/
+		default:
+			log.Println("default")
 		}
 
 	}
+	}()
+	return res
+}
+
+func draw(s chan string) {
+	x, y := 0, 10
+	X, _ := termbox.Size()
+	cb := termbox.CellBuffer()
+	go func() {
+		for {
+			switch ev := <-s {
+			case "insert":
+				if x == X {
+					copy(cb, cb[X:])
+					x = 0
+				} 
+				termbox.SetCell(x, y, ev.Ch, termbox.ColorWhite, termbox.ColorDefault)
+				x += 1
+				termbox.Flush()
+
+			}
+
+		}
+	}()
+}
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	logfile, err := os.Create("log")
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.SetOutput(logfile)
+	defer termbox.Close()
+
+	termbox.SetInputMode(termbox.InputEsc)
+
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	termbox.Flush()
+
+
+	buf := make([]rune, 1024*1024, 1024*1024)
+	c := time.After(10 * time.Second)
+
 }
